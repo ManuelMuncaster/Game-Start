433ea09c0a45223a3db399ce18aee4b2ca2d0e29
ManuelMuncaster <russmunc061@ed.amdsb.ca>
2016-09-16 13:01:44 -0400

    Add project files.

---
+++ Game Start/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Game_Start
+{
+    partial class gamescreenState
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonstartState = new System.Windows.Forms.Button();
+            this.gamestartsState = new System.Windows.Forms.Label();
+            this.numberState = new System.Windows.Forms.Label();
+            this.gotextState = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonstartState
+            // 
+            this.buttonstartState.Location = new System.Drawing.Point(86, 100);
+            this.buttonstartState.Name = "buttonstartState";
+            this.buttonstartState.Size = new System.Drawing.Size(111, 51);
+            this.buttonstartState.TabIndex = 0;
+            this.buttonstartState.Text = "Press  to Start";
+            this.buttonstartState.UseVisualStyleBackColor = true;
+            this.buttonstartState.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gamestartsState
+            // 
+            this.gamestartsState.AutoSize = true;
+            this.gamestartsState.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gamestartsState.ForeColor = System.Drawing.Color.White;
+            this.gamestartsState.Location = new System.Drawing.Point(41, 110);
+            this.gamestartsState.Name = "gamestartsState";
+            this.gamestartsState.Size = new System.Drawing.Size(156, 25);
+            this.gamestartsState.TabIndex = 1;
+            this.gamestartsState.Text = "Game Starts in...";
+            this.gamestartsState.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numberState
+            // 
+            this.numberState.AutoSize = true;
+            this.numberState.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberState.ForeColor = System.Drawing.Color.White;
+            this.numberState.Location = new System.Drawing.Point(194, 110);
+            this.numberState.Name = "numberState";
+            this.numberState.Size = new System.Drawing.Size(23, 25);
+            this.numberState.TabIndex = 2;
+            this.numberState.Text = "3";
+            // 
+            // gotextState
+            // 
+            this.gotextState.AutoSize = true;
+            this.gotextState.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gotextState.ForeColor = System.Drawing.Color.White;
+            this.gotextState.Location = new System.Drawing.Point(102, 110);
+            this.gotextState.Name = "gotextState";
+            this.gotextState.Size = new System.Drawing.Size(0, 25);
+            this.gotextState.TabIndex = 3;
+            // 
+            // gamescreenState
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.gotextState);
+            this.Controls.Add(this.numberState);
+            this.Controls.Add(this.gamestartsState);
+            this.Controls.Add(this.buttonstartState);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "gamescreenState";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonstartState;
+        private System.Windows.Forms.Label gamestartsState;
+        private System.Windows.Forms.Label numberState;
+        private System.Windows.Forms.Label gotextState;
+    }
+}
+
---
+++ Game Start/Form1.cs
@@ -0,0 +1,106 @@
+﻿/* Created by: Manuel Muncaster
+ * Date: September 8, 2016
+ * Purpose: Create a game start screen
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+
+namespace Game_Start
+{
+    public partial class gamescreenState : Form
+    {
+        public gamescreenState()
+        {
+            InitializeComponent();
+            gamestartsState.Visible = false;
+            numberState.Visible = false;
+            //gotextState.Visible = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            SoundPlayer player = new SoundPlayer(Properties.Resources.alert);
+
+            player.Play();
+            buttonstartState.Visible = false;
+            gamestartsState.Visible = true;
+            numberState.Visible = true;
+            Refresh();
+
+            Thread.Sleep(1250);
+            numberState.Text = "2";
+            Refresh();
+
+            Thread.Sleep(1250);
+            numberState.Text = "1";
+            Refresh();
+
+            Thread.Sleep(1240);
+            gamestartsState.Visible = false;
+            numberState.Visible = false;
+            //gotextState.Visible = true;
+
+            Graphics formGraphics = this.CreateGraphics();
+            Graphics fg= this.CreateGraphics();
+
+            Font drawFont = new Font("Times New Roman", 125, FontStyle.Bold);
+            Font ramsFont = new Font("Trebuchet", 30, FontStyle.Bold);
+            Font ramsarcadeFont = new Font("Trebuchet", 10, FontStyle.Bold);
+
+            Pen redPen = new Pen(Color.Red, 10);
+            Pen whitePen = new Pen(Color.White, 10);
+
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+            formGraphics.DrawEllipse(redPen, 40, 35, 200, 200);
+            formGraphics.FillEllipse(redBrush, 40, 35, 200, 200);
+            fg.DrawString("C", drawFont, whiteBrush, 45, 40);
+            fg.DrawString("Rams", ramsFont, whiteBrush, 115, 115);
+
+            fg.TranslateTransform(265, 90);
+            fg.RotateTransform(90);
+            fg.DrawString("Rams Arcade", ramsarcadeFont, whiteBrush, new Rectangle());
+            fg.ResetTransform();
+
+            Thread.Sleep(1250);
+            formGraphics.Clear(Color.Black);
+            Refresh();
+
+            Pen bluePen = new Pen(Color.Blue, 10);
+            Pen yellowPen = new Pen(Color.Yellow, 10);
+            formGraphics.DrawLine(bluePen, 200, 150, 50, 100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
